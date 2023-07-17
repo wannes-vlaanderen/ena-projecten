4ecce5e671b9255f0b6e993a684731d36b8bd83d
wannes-vlaanderen <138599016+wannes-vlaanderen@users.noreply.github.com>
2023-07-17 11:10:03 +0200

    Update filters.js

--- filters.js
+++ filters.js
@@ -338,5 +338,18 @@ function setupFilter(filterSettings) {
   applyFilters();
   filters();
   removeFiltersButton();
+  // Modal - popup for filtering results
+  const filterResults = document.getElementById('filterResults');
+  const exitButton = document.getElementById('exitButton');
+  const modal = document.getElementById('modal');
+    
+  filterResults.addEventListener('click', () => {
+    modal.classList.remove('hide-visually');
+    modal.classList.add('z5');
+  });
+    
+  exitButton.addEventListener('click', () => {
+    modal.classList.add('hide-visually');
+  });
 }
 
